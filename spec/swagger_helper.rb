4a6166d81db88dd6ad65041cce262030121fd0ed
mdziardziel <mdziardziel@gmail.com>
2020-03-21 14:27:09 +0100

    add instruction

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -17,7 +17,22 @@ RSpec.configure do |config|
       swagger: "2.0",
       info: {
         title: 'API V1',
-        version: 'v1'
+        version: 'v1',
+        description: "Instrukcja: \n" \
+          "1. Utwórz konto przez `post /api/users` \n" \
+          "2. Uwierzytelnij się używając danych podanych podczas rejestracji przez `post /api/authentication`.\n" \
+          "\t Otrzymasz unikalny JSON Web Token (jwt) z którego pomocą będziesz mógł się autoryzować podczas wykonywania zapytań.\n" \
+          "\t Aby to zrobić, skopiuj otrzymany token i podawaj w nagłówku `Authorization`. \n" \
+          "\t Token jest ważny przez 100 lat, aby go przedawnić odpytaj kolejny raz punkt końcowy dostępny pod `post /api/authentication`.\n" \
+          "\t Otrzymasz nowy token, a stary będzie nieaktualny.\n" \
+          "3. Możesz stworzyć wiele klubów, aby tego dokonać wykonaj `post /api/clubs/`.\n" \
+          "\t Zoastaniesz automatycznie gratyfikowany rolą `PRESIDENT`.\n" \
+          "\t Po stworzeniu klubu otrzymasz token, dzięki któremu inni uczestnicy będą mogli dołączyć do klubu, " \
+          "\t jednak będziesz musiał jeszcze zaakceptować ich kandydaturę przez `post /api/clubs/{club_id}/members/{member_id}/approve`.\n" \
+          "4. Możesz dołączyć do wielu klubów, aby tego dokonać wykonaj `post /api/clubs/{club_id}/members`.\n"
+          "5. Możesz wyświetlić wydarzenia/ogłoszenia, tylko dla danego klubu, lub dla wszystkich klubów.\n" \
+          "\t Wydarzenia/ogłoszenia w ścieżce /clubs/{club_id} dotyczą tylko wybranego klubu.\n" \
+          "\t Widzisz wydarzenia/ogłoszenia tylko skierowane do ciebie.\n"
       },
       paths: {}
     }
