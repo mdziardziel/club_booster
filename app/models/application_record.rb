1b5776fbcc3e29905a168e282b371b7ae8c4ea6b
mdziardziel <mdziardziel@gmail.com>
2020-04-18 21:48:06 +0200

    fix

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,7 +2,7 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
 
-  def self.trans(path, person)
+  def self.trans(path, person = nil)
     I18nForPerson.t(path, person)
   end
 end
