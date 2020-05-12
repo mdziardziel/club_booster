535a7fa138fa14ce1f2f0158fbc6899996d5ca99
mdziardziel <mdziardziel@gmail.com>
2020-05-12 19:47:19 +0200

    add age

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,13 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :name, :surname, :birth_date, :personal_description, :career_description, :email, :avatar_url
+  attributes :age, :name, :surname, :birth_date, :personal_description, :career_description, :email, :avatar_url
 
   def birth_date
     object.birth_date&.to_date  
+  end
+
+  def age
+    return nil if  object.birth_date.nil?
+    
+    ((Time.zone.now - object.birth_date.to_time) / 1.year.seconds).floor
   end
 end
