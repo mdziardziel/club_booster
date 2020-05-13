6274d3c7bf26bd7400945e338c92544a487bf8ee
mdziardziel <mdziardziel@gmail.com>
2020-05-13 18:26:25 +0200

    return 200 status when unauthorized

--- app/controllers/concerns/authorize_actions_helper.rb
+++ app/controllers/concerns/authorize_actions_helper.rb
@@ -14,7 +14,7 @@ module AuthorizeActionsHelper
   private 
 
   def not_authorized
-    render json: { message: "You are not authorized to perform this action", data: {}, errors: {}}, status: 403
+    render json: { message: "You are not authorized to perform this action", data: {}, errors: {}, status: 'UNAUTHORIZED'}, status: 200
   end
 
   def authorize_only_president_or_coach_role
