f1f156778a16f00e1af659605b758241c0c46574
mdziardziel <mdziardziel@gmail.com>
2020-05-17 14:12:41 +0200

    check if one role can manage other

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,9 +1,19 @@
 class Role
+  PRESIDENT = 'PRESIDENT'.freeze
+  COACH = 'COACH'.freeze
+  PLAYER = 'PLAYER'.freeze
   AVAILABLE_ROLES = %w(PRESIDENT COACH PLAYER)
 
   class << self
     AVAILABLE_ROLES.each do |role|
       define_method(role.downcase) { role }
     end
+  end
+
+  def self.can_manage?(manager_roles, managed_roles)
+    return true if manager_roles.include?(PRESIDENT) && managed_roles.exclude?(PRESIDENT)
+    return true if manager_roles.include?(COACH) && managed_roles.exclude?(PRESIDENT) && managed_roles.exclude?(COACH)
+
+    false
   end
 end
